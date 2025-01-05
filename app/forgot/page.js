a7acf55eeb91c52f0f918efae80a85e098c8f6c8
ArafatBytes <mdarafat1661@gmail.com>
2025-01-05 21:59:21 +0600

    added toast for reset link

--- app/forgot/page.js
+++ app/forgot/page.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Forgot() {
   const [email, setEmail] = useState("");
@@ -38,6 +38,7 @@ export default function Forgot() {
 
   return (
     <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+      <ToastContainer position="top-center" />
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-[#CB218E] to-[#6617CB] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
